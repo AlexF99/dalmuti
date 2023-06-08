c6595f87ef36da847663c1273e738d0b4669efbe
AlexF99 <alexandreopf@gmail.com>
2023-06-08 19:04:45 -0300

    sending from different machine

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ print("my port is:" + str(machine_config.get_port()))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((machine_config.ip, machine_config.get_port()))
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+# sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 while True:
     if (machine_config.get_myturn() == False):
         data, addr = sock.recvfrom(1024)
-        print("received message from: %s: %s" % addr % data)
+        print("received message:")
+        print(data, addr)
     else:
         msg = input("input: ")
         print("sending " + msg + " to:")
