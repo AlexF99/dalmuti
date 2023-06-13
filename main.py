eb26e55566c29af3181f9f777d88e5bf5bca221c
AlexF99 <alexandreopf@gmail.com>
2023-06-13 06:27:00 -0300

    removendo de vez

--- main.py
+++ main.py
@@ -134,8 +134,6 @@ while True:
                     player.mycards.remove(choice)
                     i = i + 1
 
-                player.mycards = [] #remove isso depois pelo amor de deus
-
                 if len(player.mycards) == 0:
                     print(player.mycards)
                     print("\n\nParabéns! Você ganhou essa mao!")
